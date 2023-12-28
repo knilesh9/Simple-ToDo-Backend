e4b7c4cd50a0d6b8a3f97c12a372e80b67751b4b
knilesh9 <neutron_stays@protonmail.com>
2023-12-28 23:38:14 +0530

    Done GET Just need to randomise random

---
+++ ToDo.js
@@ -0,0 +1,54 @@
+//A simple todo application with in mem data.
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+let autoGenNum = Math.random().toString(36).substring(2,6);
+
+let myTodoArray = [
+    {   
+        id: autoGenNum,
+        title: "First todo",
+        description: "this is just the first to do in the description"
+    },
+    {
+        id: autoGenNum,
+        title: "2nd Todo",
+        description: "This is the 2nd todo in the list"
+    },
+    {
+        id: "here23",
+        title: "2nd Todo",
+        description: "This is the 2nd todo in the list"
+    }
+
+
+];
+
+app.get("/todos", (req, res) => {
+    res.status(200).json({
+        myTodoArray
+    })
+})
+
+app.get("/todos/:id", (req, res) => {
+
+    let isID = myTodoArray.find((array) => array.id === req.params.id);
+
+    if(isID){
+        res.status(200).json({
+            isID
+        })
+    }else{
+        res.status(404).json({
+            myTodoArray
+        });
+    }
+})
+
+app.listen(3000);
