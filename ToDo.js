9afb65cd36ad1a473784391345a0aede39ec568d
knilesh9 <neutron_stays@protonmail.com>
2023-12-29 16:55:21 +0530

    Done

--- ToDo.js
+++ ToDo.js
@@ -19,13 +19,13 @@ let myTodoArray = [
     },
     {
         id: Math.random().toString(36).substring(2,6),
-        title: "3nd Todo",
+        title: "2nd Todo",
         description: "This is the 2nd todo in the list",
         completed: false
     },
     {
-        id: "here23",
-        title: "2nd Todo",
+        id: "the3rd", //for debugging purposes
+        title: "3rd Todo",
         description: "This is the 3rd todo in the list",
         completed: false
     }
@@ -67,6 +67,42 @@ app.post("/todos", (req, res) => {
     res.status(201).json({
         myTodoArray
     });
+})
+
+app.put("/todos/:id", (req, res) => {
+
+    let id = req.params.id
+
+    let arrayIndex = myTodoArray.findIndex( array => array.id === req.params.id);
+
+    // let id = Math.random().toString(36).substring(2, 6)
+    let {title, description, completed} = req.body
+
+     if(arrayIndex !== -1){
+
+        let updatedData = myTodoArray.splice(arrayIndex, 1, {
+            id, title, description, completed
+        })
+
+        res.status(200).json({
+            updatedData
+        })
+    }else{
+        res.status(404).send("ID NOT FOUND!")
+    }
+})
+
+app.delete("/todos/:id", (req, res) => {
+    let id = req.params.id;
+    let getItem = myTodoArray.findIndex(array => array.id === id)
+    if(getItem !== -1){
+        myTodoArray.splice(getItem, 1)
+        res.status(200).json({
+            myTodoArray
+        })
+    }else{
+        res.status(404).send("Check your ID")
+    }
 })
 
 
